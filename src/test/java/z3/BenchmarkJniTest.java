0400bb208e0533e87a9300dffccc9b192d1a5c1b
dbinkele <dbinkele@itemis.de>
2020-03-17 08:25:00 +0100

    Adds test

--- src/test/java/z3/BenchmarkJniTest.java
+++ src/test/java/z3/BenchmarkJniTest.java
@@ -1,28 +1,38 @@
 package z3;
 
 import com.microsoft.z3.BoolExpr;
+import org.junit.Before;
 import org.smtlib.IExpr;
 
 import java.util.stream.Stream;
 
 public class BenchmarkJniTest {
 
-    private static final int TIMES = 12;
-    private static final int NUMBER_COLORS = 2;
-    private static final int SEED = 6066;//0.0844f;
-    private static final float EDGE_PERCENT = 0.99f;//20;
-    private static final int VERTICES = 1100;
+//    private static final int TIMES = 12;
+//    private static final int NUMBER_COLORS = 2;
+//    private static final int SEED = 6066;//0.0844f;
+//    private static final float EDGE_PERCENT = 0.99f;//20;
+//    private static final int VERTICES = 1100;
+    private GraphGen graphGen;
+    private Scenario scenario;
+
+    @Before
+    public void setup(){
+        scenario = new Scenario(1200, 4, 6066, 0.0844f, 99);
+                //new Scenario(12, 2, 6066,0.99f, 1100);
+        graphGen = new GraphGen(scenario.VERTICES, scenario.EDGE_PERCENT, scenario.SEED);
+    }
 
     @org.junit.Test
     public void benchJni() {
 
         long start = System.currentTimeMillis();
         BenchmarkJni<BoolExpr> benchmark =
-                new BenchmarkJni<>(ModelMaker::makeZ3, new GraphGen(VERTICES, EDGE_PERCENT, SEED), NUMBER_COLORS);
-        Stream.generate(() -> 0).limit(TIMES)
+                new BenchmarkJni<>(ModelMaker::makeZ3, this.graphGen, scenario.NUMBER_COLORS);
+        Stream.generate(() -> 0).limit(scenario.TIMES)
                 .forEach(x -> benchmark.benchmark());
         long end = System.currentTimeMillis();
-        System.out.println("Dur: " + (end-start) / TIMES);
+        System.out.println("Dur: " + (end-start) / scenario.TIMES);
     }
 
 
@@ -30,10 +40,27 @@ public class BenchmarkJniTest {
     public void benchJsmtlib() {
         long start = System.currentTimeMillis();
         BenchmarkJni<IExpr> benchmark =
-                new BenchmarkJni<>(ModelMaker::makejSMTLIB, new GraphGen(VERTICES, EDGE_PERCENT, SEED), NUMBER_COLORS);
-        Stream.generate(() -> 0).limit(TIMES)
+                new BenchmarkJni<>(ModelMaker::makejSMTLIB, this.graphGen, scenario.NUMBER_COLORS);
+        Stream.generate(() -> 0).limit(scenario.TIMES)
                 .forEach(x -> benchmark.benchmark());
         long end = System.currentTimeMillis();
-        System.out.println("Dur: " + (end-start) / TIMES);
+        System.out.println("Dur: " + (end-start) / scenario.TIMES);
+    }
+
+
+    public static class Scenario {
+        public  final int TIMES;
+        public  final int NUMBER_COLORS;
+        public  final int SEED;
+        public  final float EDGE_PERCENT;
+        public  final int VERTICES;
+
+        public Scenario(int times, int number_colors, int seed, float edge_percent, int vertices) {
+            TIMES = times;
+            NUMBER_COLORS = number_colors;
+            SEED = seed;
+            EDGE_PERCENT = edge_percent;
+            VERTICES = vertices;
+        }
     }
 }
---
+++ src/test/java/z3/Z3Testing.java
@@ -0,0 +1,135 @@
+package z3;
+
+import com.microsoft.z3.*;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class Z3Testing {
+
+    @Test
+    public void testing() {
+        Context ctx = new Context();
+
+        ArithExpr ratNum = (ArithExpr) ctx.mkNumeral("42.03475", ctx.getRealSort());
+        ArithExpr ratNum1 = (ArithExpr) ctx.mkNumeral("34.5", ctx.getRealSort());
+        ArithExpr x1 = (ArithExpr)ctx.mkConst("x", ctx.getRealSort());
+
+        BoolExpr boolExpr = ctx.mkGe(x1, ratNum);
+        Solver solver = ctx.mkSolver();
+//        Params p = ctx.mkParams();
+//        p.add("rational_to_decimal", true);
+//        p.add("precision", 8);
+//        solver.setParameters(p);
+
+        Status status = solver.check(boolExpr);
+        System.out.println("---> " + status.toInt());
+        Model model = solver.getModel();
+        RatNum eval_x1 = (RatNum) model.evaluate(x1, false);
+        String val = eval_x1.toDecimalString(7);
+        BigDecimal bigDecimal = new BigDecimal(((double) eval_x1.getNumerator().getInt() / eval_x1.getDenominator().getInt()));
+        BigInteger bigIntDenominator = eval_x1.getBigIntDenominator();
+        BigDecimal divisor = new BigDecimal(bigIntDenominator);
+        BigDecimal divide = new BigDecimal(eval_x1.getBigIntNumerator()).divide(divisor, MathContext.DECIMAL32);
+        int z = 0;
+
+    }
+
+    @Test
+    public void floatingPointExample1() {
+        Context ctx = new Context();
+
+        System.out.println("FloatingPointExample1");
+        //Log.append("FloatingPointExample1");
+
+        FPSort s = ctx.mkFPSortDouble();
+        System.out.println("Sort: " + s);
+
+        FPNum x = (FPNum) ctx.mkNumeral("-1e1", s); /* -1 * 10^1 = -10 */
+        FPNum y = (FPNum) ctx.mkNumeral("-10", s); /* -10 */
+        FPNum z = (FPNum) ctx.mkNumeral("-1.25p3", s); /* -1.25 * 2^3 = -1.25 * 8 = -10 */
+        System.out.println("x=" + x.toString() +
+                "; y=" + y.toString() +
+                "; z=" + z.toString());
+
+        BoolExpr a = ctx.mkAnd(ctx.mkFPEq(x, y), ctx.mkFPEq(y, z));
+        check(ctx, ctx.mkNot(a), Status.UNSATISFIABLE);
+
+        /* nothing is equal to NaN according to floating-point
+         * equality, so NaN == k should be unsatisfiable. */
+        FPExpr k = (FPExpr) ctx.mkConst("x", s);
+        FPExpr nan = ctx.mkFPNaN(s);
+
+        /* solver that runs the default tactic for QF_FP. */
+        Solver slvr = ctx.mkSolver("QF_FP");
+        slvr.add(ctx.mkFPEq(nan, k));
+        if (slvr.check() != Status.UNSATISFIABLE)
+            throw new IllegalStateException();
+        System.out.println("OK, unsat:" + System.getProperty("line.separator") + slvr);
+
+        /* NaN is equal to NaN according to normal equality. */
+        slvr = ctx.mkSolver("QF_FP");
+        slvr.add(ctx.mkEq(nan, nan));
+        if (slvr.check() != Status.SATISFIABLE)
+            throw new IllegalStateException();
+        System.out.println("OK, sat:" + System.getProperty("line.separator") + slvr);
+
+        /* Let's prove -1e1 * -1.25e3 == +100 */
+        x = (FPNum) ctx.mkNumeral("-1e1", s);
+        y = (FPNum) ctx.mkNumeral("-1.25p3", s);
+        FPExpr x_plus_y = (FPExpr) ctx.mkConst("x_plus_y", s);
+        FPNum r = (FPNum) ctx.mkNumeral("100", s);
+        slvr = ctx.mkSolver("QF_FP");
+
+        slvr.add(ctx.mkEq(x_plus_y, ctx.mkFPMul(ctx.mkFPRoundNearestTiesToAway(), x, y)));
+        slvr.add(ctx.mkNot(ctx.mkFPEq(x_plus_y, r)));
+        if (slvr.check() != Status.UNSATISFIABLE)
+            throw new IllegalStateException();
+        System.out.println("OK, unsat:" + System.getProperty("line.separator") + slvr);
+    }
+
+    @Test
+    public void doubleTest() {
+        Context ctx = new Context();
+
+        System.out.println("FloatingPointExample2");
+
+        FPSort double_sort = ctx.mkFPSort(11, 53);
+        FPRMSort rm_sort = ctx.mkFPRoundingModeSort();
+
+        FPExpr y = (FPExpr) ctx.mkConst(ctx.mkSymbol("y"), double_sort);
+        FPExpr fp_val = ctx.mkFP(2, double_sort);
+        FPNum co = (FPNum) ctx.mkNumeral("-0.2", double_sort); /* -10 */
+
+        BoolExpr c1 = ctx.mkEq(y, co);
+
+        /* Generic solver */
+        Solver s = ctx.mkSolver();
+        s.add(c1);
+
+        if (s.check() != Status.SATISFIABLE)
+            throw new IllegalStateException();
+
+        System.out.println("OK, model: " + s.getModel().toString());
+        Expr evaluate = s.getModel().evaluate(y, true);
+        FPExpr y1 = (FPExpr) y;
+
+        System.out.println("OK, model: " + s.getModel().toString());
+        int zz = 0;
+
+    }
+
+    Model check(Context ctx, BoolExpr f, Status sat) {
+        Solver s = ctx.mkSolver();
+        s.add(f);
+        if (s.check() != sat)
+            throw new IllegalStateException();
+        if (sat == Status.SATISFIABLE)
+            return s.getModel();
+        else
+            return null;
+    }
+}
